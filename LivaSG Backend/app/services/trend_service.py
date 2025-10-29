634dc8f75b5c6701f8e9008b5c13af24119fbfa7
fong <h1510038@gmail.com>
2025-10-29 07:06:39 +0000

    Delete LivaSG Backend directory

--- LivaSG Backend/app/services/trend_service.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from ..domain.models import PriceRecord
-from ..repositories.interfaces import IPriceRepo
-
-class TrendService:
-    def __init__(self, prices: IPriceRepo): self.prices = prices
-    def series(self, area_id: str, months: int) -> List[PriceRecord]:
-        return self.prices.series(area_id, months)
